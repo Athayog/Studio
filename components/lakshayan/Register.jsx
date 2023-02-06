5970938e42c6fc6fbbba4dc496272f518299ae95
HarsimranBarki <harsimransinghbarki@gmail.com>
2023-02-06 16:06:05 +0530

    fix lakshayan on form

--- components/lakshayan/Register.jsx
+++ components/lakshayan/Register.jsx
@@ -303,7 +303,7 @@ function Register({ heroImage }) {
                                                   })}
                                                   name="course"
                                                   isReadOnly
-                                                  value={'Lakshayan'}
+                                                  value={'Lakshyan'}
                                                   disabled
                                              >
                                                   <option value="Space">
