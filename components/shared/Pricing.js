bf45ee6f9ed045c575ac5a33ba0fe4914577e053
HarsimranBarki <harsimransinghbarki@gmail.com>
2023-04-19 10:41:35 +0530

    removed undefined condition

--- components/shared/Pricing.js
+++ components/shared/Pricing.js
@@ -129,7 +129,7 @@ const Pricing = ({ pricing, registerTo, toRegister }) => {
      let generalCourses = pricing
           .filter((data) => data.isTrial == false)
           .filter((data) => data.isGeneral == true)
-          .filter((data) => data.sub_category == undefined)
+          // .filter((data) => data.sub_category == undefined)
           .filter((data) => !data.description.toLowerCase().includes('limited'))
           .sort((a, b) => a.description.length - b.description.length)
           .sort((a, b) => a.durationNum - b.durationNum);
