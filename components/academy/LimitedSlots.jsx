08bff554342e880b043dd22b7c3cfc350d060dcc
Harsimran <harsimran@gmail.com>
2024-01-04 13:05:47 +0530

    Fixed button

--- components/academy/LimitedSlots.jsx
+++ components/academy/LimitedSlots.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box, Button, Flex, Heading,Text,chakra } from '@chakra-ui/react'
 import LimitedBG from 'public/limitedBg.jpg'
 import Link from 'next/link'
+import EnquiryModal from '../home/Enquiry/EnquiryModal'
 function LimitedSlots() {
   return (
     <Box  bg={`url(${LimitedBG.src})`} p={6} backgroundSize='cover'>
@@ -9,9 +10,8 @@ function LimitedSlots() {
                 <Heading maxW='2xl' textAlign='center' fontSize='3xl'>Limited Slots: Act now for a transformative
 career journey with <chakra.span color='#5B7F38'>AthaYog Living.</chakra.span></Heading>
                 <Text fontSize='2xl'>Yoga is a Spiritual Science - Genuine Enlightenment Matters</Text>
-                <Link href={'https://store.athayogliving.com/cart/42707418448056:1'} passHref>
-                <Button target="_blank"  rounded='full' padding="20px 80px" color="#fff" background=" linear-gradient(101deg, #F54900 48.85%, #FDC685 90.46%)">Start Your Journey</Button>
-            </Link>
+                <EnquiryModal buttonText="Start Your Journey" backColor="linear-gradient(101deg, #F54900 48.85%, #FDC685 90.46%)"/>
+ 
             </Flex>
         </Box>
   )
