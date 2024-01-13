3db2625aacfa334bda6c9c189722765f4d59eaee
Harsimran <harsimran@gmail.com>
2024-01-13 17:10:22 +0530

    updated date

--- pages/yoga-academy/register/index.jsx
+++ pages/yoga-academy/register/index.jsx
@@ -67,7 +67,7 @@ function RegisterYoga() {
                 <ChakraProvider>
                     <Box p={10}>
                         <Box mb={6}>
-                            <Text p={2} fontSize='12px' bg='green.100' color="green.800" textTransform='uppercase' fontWeight='bold'>Weekend Batch Starting: April 17th - June 30th, 11:00 am - 4:00 pm</Text>
+                            <Text p={2} fontSize='12px' bg='green.100' color="green.800" textTransform='uppercase' fontWeight='bold'>Weekend Batch Starting: Feb 17th - May 27th, 11:00 am - 4:00 pm</Text>
                         
                         </Box>
                         <form onSubmit={handleSubmit(onSubmit)}>
