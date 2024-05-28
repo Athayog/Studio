2fbb64f65f0e17b444539fb78fb6c0757f0637aa
Athayog <77162718+Athayog@users.noreply.github.com>
2024-05-28 08:32:42 +0000

    removed comment

--- pages/api/email/send.js
+++ pages/api/email/send.js
@@ -147,7 +147,7 @@ const emailTemplate = (name, ticketid) => {
                     </tr>
                     <tr>
                       <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                        <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">Your Event Ticket is ready: Note: In the below box also you can mention one more column mentioning which category pass. .</div>
+                        <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">Your Event Ticket is ready .</div>
                       </td>
                     </tr>
                     <tr>
