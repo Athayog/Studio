30e7dd9ea61063c1e85e0f154980a85b17996422
Athayog <77162718+Athayog@users.noreply.github.com>
2024-08-17 12:55:54 +0000

    Updated worskhop

--- components/home/UpcomingWorkshops.js
+++ components/home/UpcomingWorkshops.js
@@ -7,9 +7,9 @@ import EnquiryModal from './Enquiry/EnquiryModal';
 import EnquireWorkshop from './enquireWorkshop';
 function UpcomingWorkshops() {
      const AugustData = [
-          { date: '10th & 11th', time: '11:00 AM', topic: 'Ayurveda Workshop', teacher: 'Shobith' },
+          { date: '10th & 11th', time: '11:00 AM', topic: 'Ayurveda', teacher: 'Shobith' },
           { date: '17th', time: '11:00 AM', topic: 'Prenatal & Postnatal Workshop', teacher: 'Harsha' },
-          { date: '21st', time: '11:00 AM', topic: 'Yoga For PCOS', teacher: 'Purvi' },
+          { date: '25th', time: '11:00 AM', topic: 'Yoga For PCOS', teacher: 'Purvi' },
           { date: '31st', time: '11:00 AM', topic: 'Guided Meditation For Stress Relief', teacher: 'Sushma' }
           // Add more data rows as needed
      ];
@@ -17,14 +17,14 @@ function UpcomingWorkshops() {
      const SeptemberData = [
           { date: '7th', time: '11:00 AM', topic: 'Advance Yoga Sadhana (Advance Yoga Asanas)', teacher: 'Madhu' },
           { date: '14th', time: '11:00 AM', topic: 'Pranayama With Bandha&apos;s', teacher: 'Harsha' },
-          { date: '21st', time: '11:00 AM', topic: 'Shatkarma', teacher: 'Ankush' },
-          { date: '29th', time: '11:00 AM', topic: 'Expressive Board Yoga Art Therapy', teacher: 'Purvi' }
+          { date: '21st', time: '7:00 AM', topic: 'Shatkarma', teacher: 'Ankush' },
+          { date: '29th', time: '11:00 AM', topic: 'Expressive Based Yoga Art Therapy', teacher: 'Purvi' }
           // Add more data rows as needed
      ];
      const OctoberData = [
           { date: '5th', time: '11:00 AM', topic: 'Yoga For Fertility', teacher: 'Sushma' },
           { date: '12th', time: '11:00 AM', topic: 'Alignment Workshop', teacher: 'Madhu' },
-          { date: '19th & 20th', time: '11:00 AM', topic: 'Yoga Nidra', teacher: 'Shobith' },
+          { date: '19th & 20th', time: '11:00 AM', topic: 'Antar Mouna', teacher: 'Shobith' },
           { date: '26th', time: '11:00 AM', topic: 'Arm Balancing Workshop', teacher: 'Ankush' }
           // Add more data rows as needed
      ];
@@ -46,15 +46,12 @@ function UpcomingWorkshops() {
                               </chakra.span>
                          </Text>
                          <Text pr={10}>
-                              Yog Nidra (Single Session) -{' '}
+                              Ayurveda and Antar mouna -{' '}
                               <chakra.span fontWeight="bold" color="purple.700">
-                                   ₹1200
+                                  Single Session -  ₹1200
                               </chakra.span>
-                         </Text>
-                         <Text pr={10}>
-                              Yog Nidra (Both Session) -{' '}
                               <chakra.span fontWeight="bold" color="purple.700">
-                                   ₹1800
+                                  Both Session -  ₹1800
                               </chakra.span>
                          </Text>
                     </Flex>
